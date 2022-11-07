de3c8f42db610177e19679d6a1548ff5579dbafc
hankei6km <gh-hankei6km@outlook.jp>
2022-11-07 11:29:29 +0000

    Implement

---
+++ gen-niotice.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat <<EOF
+🚀 $(tput setaf 2)DevContainer で実行しています。$(tput sgr0)
+$(tput setaf 6)
+ #####                                                    
+#     #  ####  #    # #####   ##   # #    # ###### #####  
+#       #    # ##   #   #    #  #  # ##   # #      #    # 
+#       #    # # #  #   #   #    # # # #  # #####  #    # 
+#       #    # #  # #   #   ###### # #  # # #      #####  
+#     # #    # #   ##   #   #    # # #   ## #      #   #  
+ #####   ####  #    #   #   #    # # #    # ###### #    # 
+$(tput sgr0)
+📗 $(tput setaf 3)利用方法(Ctrl + Click)$(tput sgr0): $(tput setab 4) README.md $(tput sgr0)
+
+EOF
